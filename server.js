6a131c51cc0158941d41632b189ed47de871e59d
Archan Datta <archan@therounds.com>
2022-01-13 12:17:47 -0400

    small db connection

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('sqlite:mydb.db');
+
+const Recipe = sequelize.define(
+	'recipes',
+	{
+		id: { field: 'recipe_id', type: Sequelize.INTEGER, primaryKey: true },
+		title: Sequelize.STRING,
+	},
+	{
+		timestamps: false,
+	}
+);
+
+// create a GET route
+app.get('/recipe', async (req, res) => {
+	try {
+		await sequelize.authenticate();
+		console.log('Connection has been established successfully.');
+	} catch (error) {
+		console.error('Unable to connect to the database:', error);
+	}
+	try {
+		const recipe = await Recipe.findAll();
+		res.json(recipe);
+	} catch (e) {
+		console.info(e);
+	}
+});
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
