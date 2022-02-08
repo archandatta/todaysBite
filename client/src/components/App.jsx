b7d59158aff7b0e8995ae7d9901eaec65fe06dfe
Archan Datta <archan@therounds.com>
2022-02-08 10:17:08 -0500

    step up react routing

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Router from './Router';
+
+// TODO: recoil initialization
+
+const App = () => {
+	return <Router />;
+};
+
+export default App;
