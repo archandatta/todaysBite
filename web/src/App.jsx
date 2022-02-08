2b1adf7d3e1a47f13f99dc84a4a2abe4028ce3c8
Archan Datta <archan@therounds.com>
2022-02-08 15:59:12 -0500

    add react suite

---
+++ web/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'rsuite/dist/rsuite.min.css';
+import { Button } from 'rsuite';
+
+const App = () => {
+	return (
+		<div className="App">
+			<Button appearance="primary"> Hello world </Button>
+		</div>
+	);
+};
+
+export default App;
